6d3855228df9bb4add0223a4db6caf06963f91a0
Sena Akbulut <34038741+Senakbulut@users.noreply.github.com>
2021-02-28 18:47:47 +0300

    Update main.js

--- blog/main.js
+++ blog/main.js
@@ -22,8 +22,10 @@ Vue.component("listElement", {
     <img :src="image" alt="">
     <div class="content">
         <h2>{{header}}</h2>
+        <slot name="date"></slot>
         <p>{{text}}</p>
         <button @click="removeElement">Listeden çıkar</button>
+        <slot name="entryLink"></slot>
     </div>
 </li>`,
   methods: {
@@ -33,32 +35,45 @@ Vue.component("listElement", {
   },
 });
 new Vue({
-  el: "#app",
+el: "#app",
   data:{
-      listElement:[
-          {
-              id:1,
-              image:'https://placeimg.com/350/150/any/1',
-              header:'Bu benim birinci basliğim',
-              text:'Bu da benim birinci yazdiğim metin'
+      selected:'listView'
+  },
+  components:{
+    'newEntry':{
+      template:'#newEntry'
+    },
+    'listView':{
+      template:'#home',
+      data() {
+        return {
+          listElement:[
+            {
+                id:1,
+                image:'https://placeimg.com/350/150/any/1',
+                header:'Bu benim birinci basliğim',
+                text:'Bu da benim birinci yazdiğim metin'
+            },
+            {
+              id:2,
+              image:'https://placeimg.com/350/150/any/2',
+              header:'Bu benim ikinci basliğim',
+              text:'Bu da benim ikinci yazdiğim metin'
           },
           {
-            id:2,
-            image:'https://placeimg.com/350/150/any/2',
-            header:'Bu benim ikinci basliğim',
-            text:'Bu da benim ikinci yazdiğim metin'
-        },
-        {
-            id:3,
-            image:'https://placeimg.com/350/150/any/3',
-            header:'Bu benim üçüncü basliğim',
-            text:'Bu da benim üçüncü yazdiğim metin'
+              id:3,
+              image:'https://placeimg.com/350/150/any/3',
+              header:'Bu benim üçüncü basliğim',
+              text:'Bu da benim üçüncü yazdiğim metin'
+          }
+        ]
         }
-      ]
-  },
-  methods:{
-    removeFromList(id){
-      this.listElement = this.listElement.filter(element => element.id !== id)
+      },
+      methods:{
+        removeFromList(id){
+          this.listElement = this.listElement.filter(element => element.id !== id)
+        }
+      },
     }
   }
 });
